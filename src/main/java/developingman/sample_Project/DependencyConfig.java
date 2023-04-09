d2c3adeb8f8a59b17f4fbf68f1986aafa9c541c5
mingyunLEE <leemingyun@naver.com>
2023-04-10 00:45:35 +0900

    Fix DependencyConfig class
    add annotation

--- src/main/java/developingman/sample_Project/DependencyConfig.java
+++ src/main/java/developingman/sample_Project/DependencyConfig.java
@@ -4,19 +4,25 @@ import developingman.sample_Project.member.MemberRepository;
 import developingman.sample_Project.member.MemberService;
 import developingman.sample_Project.snack.SnackRepository;
 import developingman.sample_Project.snack.SnackService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DependencyConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberService(new MemberRepository());
     }
+    @Bean
     public MemberRepository memberRepository() {
         return new MemberRepository();
     }
 
+    @Bean
     public SnackService snackService(){
         return new SnackService(new SnackRepository());
     }
-
+    @Bean
     public SnackRepository snackRepository() {
         return new SnackRepository();
     }

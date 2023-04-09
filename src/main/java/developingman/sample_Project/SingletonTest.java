e5650a25f673ddeee73cb01d355e737f3360eaab
mingyunLEE <leemingyun@naver.com>
2023-04-10 00:51:29 +0900

    Fix SingletonTest class
    Modifying code to test singleton containers

--- src/main/java/developingman/sample_Project/SingletonTest.java
+++ src/main/java/developingman/sample_Project/SingletonTest.java
@@ -1,15 +1,19 @@
 package developingman.sample_Project;
 
+import developingman.sample_Project.member.MemberService;
 import developingman.sample_Project.singleton.SingletonService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
-    static SingletonService singletonService1 = SingletonService.getInstance();
-    static SingletonService singletonService2 = SingletonService.getInstance();
+    static AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(DependencyConfig.class);
+
+    static MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+    static MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
     public static void main(String[] args){
-        System.out.println("memberService1 : " + singletonService1);
-        System.out.println("memberService2 : " + singletonService2);
+        System.out.println("memberService1 : " + memberService1);
+        System.out.println("memberService2 : " + memberService2);
 
     }
 }
